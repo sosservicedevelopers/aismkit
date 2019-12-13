3ded7a24d76e15932ad14f70cc2375ed13131cf7
talnur <talnur@LAPTOP330>
2019-12-13 14:44:48 +0600

    без коментариев

---
+++ AisMKIT/Models/MediaEntities.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AisMKIT.Models
+{
+    public class MediaEntities { }
+    public class DictControlType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+
+    }
+
+    public class DictLangMediaType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+    }
+
+    public class DictLegalForm
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+
+    }
+
+    public class DictMediaType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+
+    }
+
+    public class DictRegion
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+    }
+
+}
