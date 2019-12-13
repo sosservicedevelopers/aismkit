3f1ae1fd2d28ab932206e805fa449c0dd52118ef
eseptehelp <eseptehelp@gmail.com>
2019-12-13 17:41:08 +0300

    Справочники и Реестры разделены на MediaDicts и MediaEntities соответственно (то есть модели для них)

---
+++ AisMKIT/Models/MediaDicts.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AisMKIT.Models
+{
+
+    public class DictDistrict
+    {
+
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+        public string City { get; set; }
+    }
+
+
+
+    public class DictMediaFreqRelease
+    {
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+    }
+
+
+
+    public class DictMediaFinSource
+    {
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+    }
+
+    public class DictAgencyPerm
+    {
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+    }
+
+
+    public class DictMediaControlResult
+    {
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+    }
+
+    public class DictMediaSuitPerm
+    {
+        public int Id { get; set; }
+        public string NameKyrg { get; set; }
+        public string NameRus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Enum Status { get; set; }
+        public DateTime DeactiveDate { get; set; }
+        public int UsersId { get; set; }
+    }
+
+
+
+    public class DictRegion
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime CreateDate;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+    }
+
+
+
+    public class DictControlType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime DateTime;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+
+    }
+
+    public class DictLangMediaType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime CreateDate;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+    }
+
+    public class DictLegalForm
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime CreateDate;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+
+    }
+
+    public class DictMediaType
+    {
+        public int Id;
+        public string NameKyrg;
+        public string NameRus;
+        public DateTime CreateDate;
+        public Enum Status;
+        public DateTime DeactiveDate;
+        public int UsersId;
+    }
+
+}
--- AisMKIT/Models/MediaEntities.cs
+++ AisMKIT/Models/MediaEntities.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AisMKIT.Models
 {
@@ -19,24 +20,39 @@ namespace AisMKIT.Models
         public DateTime RegistrationDate { get; set; }
 
         public int DictLangMediaTypeId { get; set; }
+
         public int DictMediaTypeId { get; set; }
+
         public string AddressRus { get; set; }
+
         public string AddressKyrg { get; set; }
+
         public int DictDistrictId { get; set; }
+
         public int DictMediaFreqReleaseId { get; set; }
+
         public int DictMediaFinSourceId { get; set; }
+
         public DateTime ReregistrationDate { get; set; }
+
         public DateTime EliminationDate { get; set; }
+
         public int NumberOfPermission { get; set; }
+
         public DateTime PermissionDate { get; set; }
+
         public DateTime DateOfPay { get; set; }
 
         public string NumOfPermGas { get; set; }
         public DateTime PermGASDate { get; set; }
+
         public DateTime PermElimGASDate { get; set; }
+
         public int DictAgencyPermId { get; set; }
+
         public int UsersId { get; set; }
     }
+
 
     public class ListOfMediaHistory
     {
@@ -45,6 +61,32 @@ namespace AisMKIT.Models
 
     public class ListOfControlMedia
     {
-
+        public string Id { get; set; }
+        public string ListOfMediaId { get; set; }
+        public string DictControlTypeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDatePeriod { get; set; }
+        public DateTime EndDatePeriod { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string PatronicName { get; set; }
+        public DateTime ActDateControl { get; set; }
+        public string NumberOfWarning { get; set; }
+        public DateTime WarningDate { get; set; }
+        public DateTime WarningEndDate { get; set; }
+        public DateTime DateOfPenalty { get; set; }
+        public string DocNumPenalty { get; set; }
+        public string PenaltySum { get; set; }
+        public DateTime DateOfPenaltyPay { get; set; }
+        public DateTime AnulmentDate { get; set; }
+        public string NumberOfAnulment { get; set; }
+        public DateTime DateOfSuit { get; set; }
+        public string NumberOfSuit { get; set; }
+        public DateTime DateOfSuitPerm { get; set; }
+        public string NumberOfSuitPerm { get; set; }
+        public string DictMediaSuitPermId { get; set; }
+        public string UsersId { get; set; }
     }
+
 }
